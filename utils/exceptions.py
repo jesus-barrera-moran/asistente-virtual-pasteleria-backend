97c9c341d0372660194b450e20bd06788513d7cb
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-07 11:26:45 -0600

    Traducir mensajes de error

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -2,54 +2,54 @@ from fastapi import HTTPException, status
 
 COULD_NOT_VALIDATE_CREDENTIALS_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Could not validate credentials",
+    detail="No se pudieron validar las credenciales",
     headers={"WWW-Authenticate": "Bearer"}
 )
 
 USERNAME_ALREADY_REGISTERED_EXCEPTION = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Username already registered"
+    detail="El nombre de usuario ya está registrado"
 )
 
 INACTIVE_USER_EXCEPTION = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Inactive user"
+    detail="Usuario inactivo"
 )
 
 INVALID_ROLE_EXCEPTION = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Invalid role"
+    detail="Rol no válido"
 )
 
 FILE_NOT_FOUND_EXCEPTION = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail="File not found in general configuration"
+    detail="Archivo no encontrado en la configuración general"
 )
 
 PERMISSION_DENIED_EXCEPTION = HTTPException(
     status_code=status.HTTP_403_FORBIDDEN,
-    detail="You do not have permission to perform this action"
+    detail="No tienes permiso para realizar esta acción"
 )
 
 CREDENTIALS_REQUIRED_EXCEPTION = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Username and password are required"
+    detail="El nombre de usuario y la contraseña son obligatorios"
 )
 
 INCORRECT_CREDENTIALS_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Incorrect username or password",
+    detail="Nombre de usuario o contraseña incorrectos",
     headers={"WWW-Authenticate": "Bearer"}
 )
 
 NOT_ALLOWED_ACTION_EXCEPTION = HTTPException(
     status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-    detail="Not allowed action"
+    detail="Acción no permitida"
 )
 
 NOT_FOUND_USER_EXCEPTION = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
-    detail="User not found"
+    detail="Usuario no encontrado"
 )
 
 def INTERNAL_SERVER_ERROR_EXCEPTION(e: Exception) -> HTTPException:
