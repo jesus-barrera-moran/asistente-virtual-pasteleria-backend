46a9e664c708146e1134224f79e98826dafddbeb
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-08 16:54:02 -0600

    Revert "Implementar uso de bases de datos de clientes"
    This reverts commit 0526fcc3fcabaf9b8df36458434443d5041f59fe.

--- tools/agente_inventario_db/tool.py
+++ tools/agente_inventario_db/tool.py
@@ -1,14 +1,10 @@
-from uuid import UUID
-
 from langchain.agents import Tool
 from agents.sql_agent.agent import SQLAgent
 from llms.gpt_4o.llm import llm
-from tools.inventario_db.tool import tool
+from tools.inventario_db.tool import db
 
-async def tool(id_pasteleria: UUID):
-    db_tool = tool(id_pasteleria)
-    return Tool(
-        name="inventory_sql_database_agent",
-        func=SQLAgent(llm=llm, db=db_tool).get_agent().run,
-        description="Useful when you need to answer questions about inventory SQL database."
-    )
+tool = Tool(
+    name="inventory_sql_database_agent",
+    func=SQLAgent(llm=llm, db=db).get_agent().run,
+    description="Useful when you need to answer questions about inventory SQL database."
+)
--- tools/transacciones_db/tool.py
+++ tools/transacciones_db/tool.py
@@ -1,42 +1,26 @@
-from uuid import UUID
-
 from langchain.agents import Tool
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from llms.gpt_4o.llm import llm
+import os
 
-from services.pastries_database import obtener_bases_datos_por_pasteleria
+name=os.environ["TRANSACTIONS_DATABASE_NAME"]
+instance_connection_name = os.environ["DATABASE_INSTANCE_CONNECTION_NAME"]
+user = os.environ["DATABASE_INSTANCE_USER"]
+password = os.environ["DATABASE_INSTANCE_PASSWORD"]
 
-async def tool(id_pasteleria: UUID):
-    # Get the database name, user and password from environment variables
-    name = "transacciones"
+# Create the connection string
+conn_str = f"postgresql+pg8000://{user}:{password}@localhost:5432/{name}"
 
-    bases_de_datos = await obtener_bases_datos_por_pasteleria(id_pasteleria)
+# Create the SQLDatabase instance using the connection string
+db = SQLDatabase.from_uri(conn_str)
 
-    bases_de_datos_inventario = [base_de_datos for base_de_datos in bases_de_datos if base_de_datos["nombre"] == name]
+# Crear la cadena SQLDatabaseChain usando la base de datos en la nube
+sql_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
 
-    if not bases_de_datos_inventario:
-        raise Exception(f"Database {name} not found for bakery {id_pasteleria}")
-    
-    user = bases_de_datos_inventario[0]["usuario"]
-    password = bases_de_datos_inventario[0]["clave"]
-    host = bases_de_datos_inventario[0]["servidor"]
-    puerto = bases_de_datos_inventario[0]["puerto"]
-
-    # Create the connection string
-    conn_str = f"postgresql+pg8000://{user}:{password}@{host}:{puerto}/{name}"
-
-    # Create the SQLDatabase instance using the connection string
-    db = SQLDatabase.from_uri(conn_str)
-
-    # Crear la cadena SQLDatabaseChain usando la base de datos en la nube
-    sql_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
-
-    # Crear la herramienta usando la cadena SQLDatabaseChain
-    tool = Tool(
-        name="transactions_db",
-        func=sql_chain.run,
-        description="Useful when you need to answer questions about transactions' database."
-    )
-
-    return tool
+# Crear la herramienta usando la cadena SQLDatabaseChain
+tool = Tool(
+    name="transactions_db",
+    func=sql_chain.run,
+    description="Useful when you need to answer questions about transactions' database."
+)
